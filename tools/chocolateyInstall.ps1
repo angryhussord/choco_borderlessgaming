dc8f40b0db7e766e56bc5283f31278621c3c2f48
angryhussord <angryhussord@outlook.com>
2015-07-31 15:58:17 -0700

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'borderlessgaming' 
+$installerType = 'exe' 
+$url = 'https://github.com/Codeusa/Borderless-Gaming/releases/download/8.2/BorderlessGaming_8.2__setup.exe' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
