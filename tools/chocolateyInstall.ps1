a28c52d48cf2618bd7492988d977d3665e327ba1
Patrick Hufford <pahuff@microsoft.com>
2017-04-17 05:40:39 -0700

    update for 9.1 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,20 @@
-﻿$packageName = 'borderlessgaming' 
-$installerType = 'exe' 
-$url = 'https://github.com/Codeusa/Borderless-Gaming/releases/download/8.4/BorderlessGaming_8.4__setup.exe' 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-$validExitCodes = @(0) 
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageName = 'borderlessgaming'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+$installerType = 'EXE'
+$url = 'https://github.com/Codeusa/Borderless-Gaming/releases/download/9.1/BorderlessGaming_9.1__setup.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+  softwareName  = 'borderlessgaming*'
+  checksum      = 'EDDBB4BCEF68647B77B3E64A31A889DDE6F0964654803D924E1A9D3527CEFB89'
+  checksumType  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;
